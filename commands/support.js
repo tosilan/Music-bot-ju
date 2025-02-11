4b974501e37dfcb9e4542717a85dfa16ffa7868b
YJ <104505841+tosilan@users.noreply.github.com>
2025-02-11 14:51:46 +0900

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -4,15 +4,14 @@ const musicIcons = require('../UI/icons/musicicons.js');
 
 module.exports = {
     name: "support",
-    description: "Get support server link",
+    description: "サポートサーバーのリンクを取得します",
     permissions: "0x0000000000000800",
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "https://discord.gg/N28YUYpdAe";
+            const githubLink = "https://www.ykgm.f5.si";
+            const youtubeLink = "https://youtube.com/channel/UCN3fR6nfUZIUa_zwkGq6f2Q?si=ynz2H8FhuOcauAbi";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
@@ -22,10 +21,9 @@ module.exports = {
                     url: config.SupportServer
                 })
                 .setDescription(lang.support.embed.description
-                    .replace("{supportServerLink}", supportServerLink)
-                    .replace("{githubLink}", githubLink)
-                    .replace("{replitLink}", replitLink)
-                    .replace("{youtubeLink}", youtubeLink)
+                    .replace("{supportServerLink}", サポートサーバー)
+                    .replace("{githubLink}", 公式サイト)
+                    .replace("{youtubeLink}", YouTube)
                 )
                 .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
                 .setTimestamp();
