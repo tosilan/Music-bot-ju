4c0a433d54096fabc596a3702e775e4dab9121c8
YJ <104505841+tosilan@users.noreply.github.com>
2025-02-11 18:03:38 +0900

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 const { Riffy } = require("riffy");
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, AttachmentBuilder, PermissionsBitField } = require("discord.js");
-const { queueNames, requesters } = require("./commands/play");
+const { queueNames, requesters } = require("./commands/y-play");
 const config = require("./config.js");
 const musicIcons = require('./UI/icons/musicicons.js');
 const colors = require('./UI/colors/colors');
